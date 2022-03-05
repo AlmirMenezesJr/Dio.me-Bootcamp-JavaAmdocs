efc2173676059d3f05f8514408e294d79a617194
AlmirMenezesJr <jrjmswii@gmail.com>
2022-03-05 07:51:21 -0300

    mudança na pasta e add novos projetos do bootcamp

---
+++ dio-dominando-IDE-projetos/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+public class PrimeiroPrograma {
+	public static void main (String args[]) {
+		int a = 2;
+		int b = 3;
+		System.out.println("Hello, World!!!");
+		System.out.println("a variavel (a) vale: " + a);
+		System.out.println("a variavel (b) vale: " + b);
+		System.out.println("a soma de (a) e (b) e:" + (a+b));
+	}
+}
